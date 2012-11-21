4ce642b4ed482684f1b43d59d98c360b927d996f
Xihui <ztyaner11@gmail.com>
2012-11-21 10:27:41 -0500

    changes 2 in branch1 with conflict

--- src/xihui/codepractice/HelloBranch.java
+++ src/xihui/codepractice/HelloBranch.java
@@ -3,5 +3,5 @@ package xihui.codepractice;
 public class HelloBranch {
 	//changes in branchs
 	//changes from working directory 1
-	//CHanges 2 in branch testing before rebase
+	//Changes in branch1 before rebase
 }
--- src/xihui/codepractice/HelloMaster.java
+++ src/xihui/codepractice/HelloMaster.java
@@ -1,5 +1,5 @@
 package xihui.codepractice;
 
 public class HelloMaster {
- //changes before rebasing in branch1
+ //changes before rebasing in branch1 to be conflict
 }
