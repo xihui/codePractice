127aa948ea62947d8a48cd4f7d1bfa320a5eed91
Xihui <ztyaner11@gmail.com>
2012-11-21 10:54:35 -0500

    changes in master before merge

--- src/xihui/codepractice/HelloBranch.java
+++ src/xihui/codepractice/HelloBranch.java
@@ -1,7 +1,5 @@
 package xihui.codepractice;
 
 public class HelloBranch {
-	//changes in branchs
-	//changes from working directory 1
-	//Changes in branch1 before rebase
+	 //Changes in master before merge
 }
